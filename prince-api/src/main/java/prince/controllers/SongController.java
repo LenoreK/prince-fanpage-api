f9d04c72f12bfa4c4db25e7526ce821fbc66dc6e
DLChristian <122813971+DLChristian@users.noreply.github.com>
2023-03-23 15:24:02 -0500

    songcontroller

--- prince-api/src/main/java/prince/controllers/SongController.java
+++ prince-api/src/main/java/prince/controllers/SongController.java
@@ -1,2 +1,26 @@
-package prince.controllers;public class SongController {
+package prince.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import prince.domain.SongService;
+import prince.models.Song;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/song")
+public class SongController {
+
+    private final SongService songService;
+
+
+    public SongController(SongService songService) {
+        this.songService = songService;
+    }
+
+    @GetMapping
+    public List<Song> findAll() { return songService.findAll(); }
 }
