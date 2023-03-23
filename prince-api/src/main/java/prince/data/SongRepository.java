6c445b378d4e81afb79598d59af6d2c3d8d3f508
Lenore <lenorekelly2@gmail.com>
2023-03-23 14:09:37 -0500

    adding song

--- prince-api/src/main/java/prince/data/SongRepository.java
+++ prince-api/src/main/java/prince/data/SongRepository.java
@@ -1,6 +1,13 @@
 package prince.data;
 
+import prince.models.Song;
+
+import java.util.List;
+
 public interface SongRepository {
 
 
+    List<Song> findAll();
+
+    Song findById(int songId);
 }
---
+++ prince-api/src/test/java/prince/data/SongJdbcTemplateRepositoryTest.java
@@ -0,0 +1,32 @@
+package prince.data;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class SongJdbcTemplateRepositoryTest {
+
+    @Autowired
+    SongJdbcTemplateRepository repository;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup(){
+        knownGoodState.set();
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+
+    }
+}
