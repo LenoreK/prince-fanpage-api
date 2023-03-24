81d5306acf459721842cd00abf03e3e0f9100efe
Lenore <lenorekelly2@gmail.com>
2023-03-24 14:55:28 -0500

    adding songs

--- prince-ui/src/components/TopSongs.js
+++ prince-ui/src/components/TopSongs.js
@@ -2,8 +2,8 @@ import NavBar from "./NavBar";
 function TopSongs() {
     return (
     <>
-    <NavBar />
-    <div id="song-body">
+        <NavBar />
+        <div id="song-body">
         <h1 id="top-songs">Top Rated Songs</h1>
         <ul id="grid" class="list-group">
             <li class="list-group-item">Purple Rain</li>
@@ -16,8 +16,8 @@ function TopSongs() {
             <li class="list-group-item">Sign O' The Times</li>
             <li class="list-group-item">I Wanna Be Your Lover</li>
             <li class="list-group-item">If I Was Your Girlfirend</li>
-        </ul>
-    </div>
+            </ul>
+            </div>
     </>
     );
 }
