592c5df6be146f8276bf9890d58068630c9c6018
Lenore <lenorekelly2@gmail.com>
2023-03-24 15:10:44 -0500

    adding css

--- prince-ui/src/components/Home.js
+++ prince-ui/src/components/Home.js
@@ -17,16 +17,16 @@ function Home() {
         <div style={styles}> 
         <nav class="navbar navbar-expand">
         <div class="container-fluid">
-            <h1>Welcome to Prince Fan Page</h1>        
+            <h1 class="header">Welcome to Prince Fan Page</h1>        
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
               <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
-                <a class="nav-link" href="/albums">Albums</a>
+                <a class="nav-link active link" aria-current="page" href="/">Home</a>
+                <a class="nav-link link" href="/albums">Albums</a>
 
-                <a class="nav-link" href="/topSongs">Top Songs</a>
+                <a class="nav-link link" href="/topSongs">Top Songs</a>
   
               </div>
             </div> 
--- prince-ui/src/components/NavBar.js
+++ prince-ui/src/components/NavBar.js
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom";
 function NavBar() {
     return (
         <nav className="navbar">
-            <h1>Albums</h1>
+            <h1 class="header"> Albums</h1>
+                <Link className="nav-link link" to="/">Home</Link>
+                <Link className="nav-link link" to="/Albums">Albums</Link>
+                <Link className="nav-link link" to="/TopSongs">Top Songs</Link>
             <div className="col d-flex justify-content-end">
-                <Link to="/add" className="btn btn-primary">Add Album</Link>
+                <Link to="/add" className="btn btn-primary add">Add Album</Link>
             </div>    
         </nav>
     );
