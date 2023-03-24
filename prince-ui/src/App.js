398b2723c7e1c6806f0ec8a0e29362693a95a870
DLChristian <122813971+DLChristian@users.noreply.github.com>
2023-03-24 09:42:07 -0500

    updatejson

--- prince-ui/src/App.js
+++ prince-ui/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+
+      <Routes>
+        <Route path="/add" element={<AlbumForm />} />
+        <Route path="/edit/:id" element={<AlbumForm />} />
+        <Route path="/delete/:id" element={<Delete />} />
+        <Route path="/confirmation" element={<Confirmation />} />
+        <Route path="/error" element={<Error />} />
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
