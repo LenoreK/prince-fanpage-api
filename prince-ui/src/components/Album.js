666cac7298c3908b2b32ea880df296ce5c8ba32e
DLChristian <122813971+DLChristian@users.noreply.github.com>
2023-03-24 14:52:16 -0500

    cards with nav bar

--- prince-ui/src/components/Album.js
+++ prince-ui/src/components/Album.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { findAll } from "../service/albumService";
+import { Link } from "react-router-dom";
+import NavBar from "./NavBar";
 import AlbumCard from "./AlbumCard";
 
 export default function Album() {
@@ -14,10 +16,14 @@ export default function Album() {
             .catch(() => navigate("/error"));
     }, [navigate])
 
-    return(
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-2">
-            {albums.map(a => <AlbumCard key={a.albumId} album={a} />)}
-        </div>
+    return (
+        <>
+            <NavBar />
+            <Link to="/" className="btn btn-success my-2">Home</Link>
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-2">
+                {albums.map(a => <AlbumCard key={a.albumId} album={a} />)}
+            </div>
+        </>
     );
-    
+
 }
