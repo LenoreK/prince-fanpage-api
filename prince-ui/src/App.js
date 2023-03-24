444bfc8df52fd2202edd376ed0cb2cc7936b55b3
DLChristian <122813971+DLChristian@users.noreply.github.com>
2023-03-24 10:09:49 -0500

    ui

--- prince-ui/src/App.js
+++ prince-ui/src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Confirmation from "./components/Confirmation";
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <NavBar />
 
-  );
+      <Routes>
+      <Route path="/add" element={<AlbumForm />} />
+      <Route path="/edit:id" element={<AlbumForm />} />
+      <Route path="/delete:id" element={<Delete />} />
+      <Route path="/confirmation" element={<Confirmation />} />
+      <Route path="/error" element={<Error />} />
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
