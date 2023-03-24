f46691c116e6f66241e7a4f885d484046a4bddc8
Lenore <lenorekelly2@gmail.com>
2023-03-24 14:46:29 -0500

    top rated song

--- prince-ui/src/components/TopSongs.js
+++ prince-ui/src/components/TopSongs.js
@@ -1,8 +1,24 @@
+import NavBar from "./NavBar";
 function TopSongs() {
     return (
-    <div>
-        <h1>Top Rated Songs</h1>
+    <>
+    <NavBar />
+    <div id="song-body">
+        <h1 id="top-songs">Top Rated Songs</h1>
+        <ul id="grid" class="list-group">
+            <li class="list-group-item">Purple Rain</li>
+            <li class="list-group-item">When Doves Cry</li>
+            <li class="list-group-item">Kiss</li>
+            <li class="list-group-item">Raspberry Beret</li>
+            <li class="list-group-item">1999</li>
+            <li class="list-group-item">Little Red Corvette</li>
+            <li class="list-group-item">Let's Go Crazy</li>
+            <li class="list-group-item">Sign O' The Times</li>
+            <li class="list-group-item">I Wanna Be Your Lover</li>
+            <li class="list-group-item">If I Was Your Girlfirend</li>
+        </ul>
     </div>
+    </>
     );
 }
 export default TopSongs;
